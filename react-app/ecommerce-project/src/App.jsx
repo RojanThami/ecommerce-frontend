cf1cb991404ad6365af6ab291b64777b8b2c234a
RojanThami <rozanmark5@gmail.com>
2026-05-10 19:57:27 +0545

    Order and Tracker html transfer into react

--- react-app/ecommerce-project/src/App.jsx
+++ react-app/ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import{Routes, Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackerPage } from './pages/TrackerPage'
 
 import './App.css'
 
@@ -10,6 +12,8 @@ function App() {
     <Routes>
         <Route index element = {<HomePage />} />
         <Route path="checkout" element = {<CheckoutPage />} />
+        <Route path="orders" element = {<OrdersPage />} />
+        <Route path="tracker" element = {<TrackerPage />} />
     </Routes>
   );
 }
